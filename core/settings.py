8561db96c93241cb80a1640840c21adbd4d200d9
KevinCorredor <kevincorredor10@gmail.com>
2023-04-25 00:21:11 -0500

    rrr

--- core/settings.py
+++ core/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
 
+ALLOWED_HOSTS =[]
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',
+    'django.contrib.messages',  
     'django.contrib.staticfiles',
     'home',
 ]
@@ -124,9 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # and creating unique names for each version so they can safely be cached forever.
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS =[
-    'static'
-]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
