4fb1549b96d68867bc74317f9a555ed33210b38e
KevinCorredor <kevincorredor10@gmail.com>
2023-04-24 23:35:58 -0500

    df

--- core/settings.py
+++ core/settings.py
@@ -117,12 +117,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-if not DEBUG:    # Tell Django to copy statics to the `staticfiles` direct°°ory
+   # Tell Django to copy statics to the `staticfiles` direct°°ory
     # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS =[
     'static'
